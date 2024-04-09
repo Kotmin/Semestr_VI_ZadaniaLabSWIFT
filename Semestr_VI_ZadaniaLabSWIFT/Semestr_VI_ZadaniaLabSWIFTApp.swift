7738f79ec01289ddfb7230a13c6d6ebdb8fdcc0c
Kotmin <70173732+Kotmin@users.noreply.github.com>
2024-04-09 17:08:34 +0200

    Initial Commit

---
+++ Semestr_VI_ZadaniaLabSWIFT/Semestr_VI_ZadaniaLabSWIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Semestr_VI_ZadaniaLabSWIFTApp.swift
+//  Semestr_VI_ZadaniaLabSWIFT
+//
+//  Created by Paweł Jan Tłusty on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Semestr_VI_ZadaniaLabSWIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
