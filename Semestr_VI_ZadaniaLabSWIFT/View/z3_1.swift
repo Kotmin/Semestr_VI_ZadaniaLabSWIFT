5faeabc09d4f0ac812d3be5b602312fe01999f4a
Kotmin <70173732+Kotmin@users.noreply.github.com>
2024-04-09 19:29:56 +0200

    add header 3.1

--- Semestr_VI_ZadaniaLabSWIFT/View/z3_1.swift
+++ Semestr_VI_ZadaniaLabSWIFT/View/z3_1.swift
@@ -11,6 +11,8 @@ struct z3_1: View {
     @State private var ileRazy:Int = 0
     
     var body: some View {
+        Text("Zadanie 3.1")
+                        .font(.largeTitle)                        .padding(.top)
         z3_1_PanelView(ileRazy: $ileRazy)
     }
 }
