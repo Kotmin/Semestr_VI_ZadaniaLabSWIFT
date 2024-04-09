8c2d115a87afa058b8b19c3ff0e7c8bbb4d71081
Kotmin <70173732+Kotmin@users.noreply.github.com>
2024-04-09 19:29:00 +0200

    main menu refactor

--- Semestr_VI_ZadaniaLabSWIFT/ContentView.swift
+++ Semestr_VI_ZadaniaLabSWIFT/ContentView.swift
@@ -11,27 +11,107 @@ struct ContentView: View {
     
     @Environment(\.navigate) private var navigate
     
+    
+    @State private var lab1Collapsed: Bool = false
+    @State private var lab3Collapsed: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "cat")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Pare zadań")
+
+            HeaderView()
             
-            Button(action: { navigate(.z1_1) }, label: {
-                Text("Z_1_1")
-            })
-            Button(action: { navigate(.z1_2) }, label: {
-                Text("Z_1_2")
-            })
-            Button(action: { navigate(.z1_5) }, label: {
-                Text("Z_1_5")
-            })
-            Button(action: { navigate(.z3_1) }, label: {
-                Text("Z_3_1")
-            })
+            SectionView(isCollapsed: $lab1Collapsed, sectionTitle: "Laboratorium 1"){
+                HStack{
+                    
+                    Button(action: { navigate(.z1_1) }, label: {
+                        Text("1.1")
+                    })
+//                    .padding()
+//                    .foregroundColor(.white)
+//                    .background(.black)
+//                    .cornerRadius(8)
+//                    .overlay(
+//                        RoundedRectangle(cornerRadius: 8)
+//                            .stroke(Color.black, lineWidth: 1)
+//                    )
+                    Button(action: { navigate(.z1_2) }, label: {
+                        Text("1.2")
+                    })
+                    Button(action: { navigate(.z1_5) }, label: {
+                        Text("1.5")
+                    })
+                    
+                }
+                
+            }
             
+            SectionView(isCollapsed: $lab3Collapsed, sectionTitle: "Laboratorium 3"){
+                HStack{
+                    
+                    Button(action: { navigate(.z3_1) }, label: {
+                        Text("3.1")
+                    })
+                    
+                }
+                
+            }
+            FooterView(creator: "Paweł Jan Tłusty")
         }
+        .padding()
+    }
+}
+
+
+struct HeaderView: View {
+    var body: some View {
+        Image(systemName: "cat")
+            .imageScale(.large)
+            .foregroundStyle(.tint)
+        Text("Parę zadań")
+    }
+}
+
+struct SectionView<Content: View>: View {
+    @Binding var isCollapsed: Bool
+    let sectionTitle: String
+    @ViewBuilder var content: Content
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(sectionTitle)
+                    .foregroundColor(.gray)
+                    .font(.headline)
+                Spacer()
+                Button(action: { isCollapsed.toggle() }) {
+                    Image(systemName: isCollapsed ? "chevron.right" : "chevron.down")
+                        .foregroundColor(.gray)
+                }
+            }.padding()
+            
+            if !isCollapsed {
+                content
+            }
+        }
+
+    }
+}
+
+
+
+struct FooterView: View {
+    var creator: String
+    
+    var body: some View {
+        VStack {
+            Text("Created by:")
+                .foregroundColor(.black)
+                .font(.caption)
+            Text(creator)
+                .foregroundColor(.black)
+                .font(.headline)
+        }
+        .frame(maxWidth: .infinity)
         .padding()
     }
 }
